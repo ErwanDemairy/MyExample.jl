5a97776cbbd77b0b4b9804cc4e4b916fff89caea
Erwan Demairy <Erwan.Demairy@inria.fr>
2023-02-02 17:18:14 +0100

    adding differentiation.

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -1,6 +1,9 @@
 module MyExample
 
+using ForwardDiff
+
 include("extra_file.jl")
 
 export my_f
+export derivative_of_my_f
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,7 @@ using Test
   @test my_f(2,3) == 13
   @test my_f(1,3) == 11 
 end
+
+@testset "Derivative Tests" begin
+  @test derivative_of_my_f(2,1) == 2
+end
